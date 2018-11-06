6776bdb329794e0128bb9d99461386559a12dafb
刘钰洋 <2490583386@qq.com>
2018-11-06 08:57:38 +0800

    goods_platform

---
+++ src/main/java/com/gp/goods_platform/GoodsPlatformApplication.java
@@ -0,0 +1,12 @@
+package com.gp.goods_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodsPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodsPlatformApplication.class, args);
+    }
+}
